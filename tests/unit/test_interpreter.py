77bbc63075eccc5e98d482a3decbd7c84ab3ecc7
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-04-22 00:05:55 -0700

    unittest: add unit test to test very long Section description

--- tests/unit/test_interpreter.py
+++ tests/unit/test_interpreter.py
@@ -1147,3 +1147,38 @@ class TestScriptBuilder:
         )
         test_script = node.testscript
         assert test_script == expected_result
+
+    @pytest.mark.parametrize(
+        ('framework', 'user_data', 'expected_result'),
+        [
+            (
+                FWTYPE.UNITTEST,
+                TESTDATA.script_builder.case3.data,
+                TESTDATA.script_builder.case3.unittest,
+            ),
+            (
+                FWTYPE.PYTEST,
+                TESTDATA.script_builder.case3.data,
+                TESTDATA.script_builder.case3.pytest,
+            ),
+            (
+                FWTYPE.ROBOTFRAMEWORK,
+                TESTDATA.script_builder.case3.data,
+                TESTDATA.script_builder.case3.robotframework,
+            ),
+        ]
+    )
+    def test_building_script_case3(self, framework, user_data, expected_result):
+        SCRIPTINFO.reset_global_vars()
+        SCRIPTINFO.reset_devices_vars()
+        SCRIPTINFO.load_testing_data()
+        node = ScriptBuilder(
+            user_data,
+            indentation=indentation,
+            framework=framework,
+            username=user_info.username,
+            email=user_info.email,
+            company=user_info.company
+        )
+        test_script = node.testscript
+        assert test_script == expected_result
