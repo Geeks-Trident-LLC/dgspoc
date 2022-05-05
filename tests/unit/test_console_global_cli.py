1a4eaa1d76a0f2daba3051f43483406796c68ea2
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-05-05 13:11:41 -0700

    unittest: complete unit test for test_console_global_cli.py

--- tests/unit/test_console_global_cli.py
+++ tests/unit/test_console_global_cli.py
@@ -41,7 +41,7 @@ class TestConsoleHelpFlag:
         assert commands == expected_result
 
     @pytest.mark.parametrize(
-        'positional_arguments',
+        'positional_argument',
         [
             ' -h, --help ',
             ' -v, --version ',
@@ -57,8 +57,8 @@ class TestConsoleHelpFlag:
             ' --template-storage '
         ]
     )
-    def test_position_command_flag(self, positional_arguments):
-        chk = positional_arguments in self.output
+    def test_position_command_flag(self, positional_argument):
+        chk = positional_argument in self.output
         assert chk is True
 
 
@@ -68,7 +68,8 @@ class TestConsoleVersionCommand:
         'cmdline',
         [
             'dgs --version',
-            # 'dgs version',      # this command works the same above
+            # 'dgs -v',
+            # 'dgs version',
         ]
     )
     def test_version_flag(self, cmdline):
@@ -80,3 +81,101 @@ class TestConsoleVersionCommand:
         match = re.match(pattern, output)
         chk = bool(match)
         assert chk is True
+
+
+class TestConsoleInfoCommand:
+    """Note: comment line will be work as same as previous"""
+
+    def test_info_help_flag(self):
+        cmdline = 'dgs info --help'
+        exit_code, output = getstatusoutput(cmdline)
+        assert exit_code == ECODE.SUCCESS
+        assert 'dgs info usage' in output
+        assert ' --all          ' in output
+        assert ' --dependency   ' in output
+        assert ' --template-storage  ' in output
+        assert ' -h, --help ' in output
+        assert 'dgs info operands [options]' in output
+        assert 'dgs info example ' in output
+
+    def test_info_command(self):
+        cmdline = 'dgs info'
+        exit_code, output = getstatusoutput(cmdline)
+
+        assert exit_code == ECODE.SUCCESS
+        assert Cli.prog_fn.title() in output
+        assert '| Project : ' in output
+        assert '| License : ' in output
+        assert '| Platform: ' in output
+
+    @pytest.mark.parametrize(
+        'cmdline',
+        [
+            'dgs info --dependency',
+            # 'dgs info dependency',
+            # 'dgs info depend',
+        ]
+    )
+    def test_info_dependency_command(self, cmdline):
+        exit_code, output = getstatusoutput(cmdline)
+
+        assert exit_code == ECODE.SUCCESS
+        assert ' Package: compare_versions ' in output
+        assert ' Package: dlapp ' in output
+        assert ' Package: pytest ' in output
+        assert ' Package: python-dateutil ' in output
+        assert ' Package: pyyaml ' in output
+        assert ' Package: regexapp ' in output
+        assert ' Package: robotframework ' in output
+        assert ' Package: templateapp ' in output
+        assert ' Package: textfsm ' in output
+
+    @pytest.mark.parametrize(
+        'cmdline',
+        [
+            'dgs info --template-storage',
+            # 'dgs info template-storage',
+            # 'dgs info template',
+            # 'dgs info storage',
+        ]
+    )
+    def test_info_dependency_command(self, cmdline):
+        exit_code, output = getstatusoutput(cmdline)
+
+        assert exit_code == ECODE.SUCCESS
+        assert ' Template Storage Info: ' in output
+        assert '   - Location: ' in output
+        assert '   - Existed: ' in output
+        assert '   - Total Templates: ' in output
+
+    @pytest.mark.parametrize(
+        'cmdline',
+        [
+            'dgs info --all',
+            # 'dgs info all',
+        ]
+    )
+    def test_info_dependency_command(self, cmdline):
+        exit_code, output = getstatusoutput(cmdline)
+
+        assert exit_code == ECODE.SUCCESS
+
+        assert Cli.prog_fn.title() in output
+        assert '| Project : ' in output
+        assert '| License : ' in output
+        assert '| Platform: ' in output
+
+        assert ' Package: compare_versions ' in output
+        assert ' Package: dlapp ' in output
+        assert ' Package: pytest ' in output
+        assert ' Package: python-dateutil ' in output
+        assert ' Package: pyyaml ' in output
+        assert ' Package: regexapp ' in output
+        assert ' Package: robotframework ' in output
+        assert ' Package: templateapp ' in output
+        assert ' Package: textfsm ' in output
+
+        assert ' Template Storage Info: ' in output
+        assert '   - Location: ' in output
+        assert '   - Existed: ' in output
+        assert '   - Total Templates: ' in output
