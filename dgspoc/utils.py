d9aa742bbce8a1ef32109f0d9e411f57b3aa1895
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-05-12 16:21:16 -0700

    baseline: improve utils.File

--- dgspoc/utils.py
+++ dgspoc/utils.py
@@ -183,6 +183,10 @@ class File:
     message = ''
 
     @classmethod
+    def clean(cls):
+        cls.message = ''
+
+    @classmethod
     def is_exist(cls, filename):
         """Check file existence
 
@@ -194,6 +198,7 @@ class File:
         -------
         bool: True if existed, otherwise False
         """
+        cls.clean()
         try:
             file_obj = Path(filename)
             return file_obj.exists()
@@ -214,6 +219,7 @@ class File:
         -------
         bool: True if created, otherwise False
         """
+        cls.clean()
         filename = cls.get_path(str(filename).strip())
         if cls.is_exist(filename):
             cls.message = 'File is already existed.'
@@ -284,6 +290,7 @@ class File:
         -------
         str: content of file
         """
+        cls.clean()
         filename = cls.get_path(file_path)
         try:
             with open(filename) as stream:
@@ -319,7 +326,7 @@ class File:
         -------
         object: YAML result
         """
-
+        cls.clean()
         yaml_result = default
 
         try:
@@ -365,6 +372,7 @@ class File:
         -------
         bool: True if successfully saved, otherwise, False
         """
+        cls.clean()
         try:
             if Misc.is_list(data):
                 content = '\n'.join(str(item) for item in data)
@@ -396,6 +404,7 @@ class File:
         -------
         bool: True if successfully deleted, otherwise, False
         """
+        cls.clean()
         try:
             filepath = File.get_path(filename)
             file_obj = Path(filepath)
