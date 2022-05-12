2dda31cd15fe7e60639f7d5f59104b10a70c3b61
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-05-12 15:17:36 -0700

    baseline: rework operation.do_build_template

--- dgspoc/operation.py
+++ dgspoc/operation.py
@@ -68,6 +68,9 @@ class OptionSelector:
                     self.method = do_build_template
                 elif feature == FEATURE.SCRIPT:
                     raise Exception('TODO: Need to implement do_build_script')
+                else:
+                    exit_code = ECODE.SUCCESS if feature == 'usage' else ECODE.BAD
+                    show_usage(self.options.command, exit_code=exit_code)
             elif self.options.command == COMMAND.SEARCH:
                 self.method = do_search_template
             elif self.options.command == COMMAND.TEST:
@@ -140,75 +143,68 @@ def do_build_template(options):
     command, operands = options.command, list(options.operands)
     op_count = len(operands)
     feature = str(operands[0]).lower().strip() if op_count > 0 else ''
-    if command == 'build' and feature == 'template':
-        operands = operands[1:]
-        name = '{}_{}'.format(command, feature)
-        validate_usage(name, operands)
-        validate_example_usage(name, operands)
 
-        op_txt = ' '.join(operands).rstrip()
+    operands = operands[1:]
+    name = '{}_{}'.format(command, feature)
+    validate_usage(name, operands)
+    validate_example_usage(name, operands)
 
-        if not op_txt:
-            show_usage(name, exit_code=ECODE.BAD)
+    op_txt = ' '.join(operands).rstrip()
 
-        if File.is_exist(op_txt):
-            with open(op_txt) as stream:
-                user_data = stream.read()
+    if not op_txt:
+        show_usage(name, exit_code=ECODE.BAD)
+
+    if File.is_exist(op_txt):
+        with open(op_txt) as stream:
+            user_data = stream.read()
+    else:
+        user_data = op_txt
+
+    try:
+        factory = TemplateBuilder(
+            user_data=user_data, author=options.author, email=options.email,
+            company=options.company
+        )
+
+        template_id = options.tmplid.strip()
+        filename = options.filename.strip()
+
+        fmt1 = '+++ Successfully uploaded generated template to "{}" template ID.'
+        fmt2 = '+++ Successfully saved generated template to {}'
+        fmt3 = 'CANT save generated template to existing {} file.  Use replaced flag accordingly.'
+
+        if template_id or filename:
+            is_ok = True
+            lst = []
+            if template_id:
+                is_uploaded = TemplateStorage.upload(
+                    template_id, factory.template, replaced=options.replaced
+                )
+                is_ok &= is_uploaded
+                msg = fmt1.format(template_id) if is_uploaded else TemplateStorage.message
+                lst.append(msg)
+            if filename:
+                filename = File.get_path(filename)
+                if File.is_exist(filename) and not options.replaced:
+                    msg = fmt3.format(filename)
+                    is_ok &= False
+                else:
+                    is_saved = File.save(options.filename, factory.template)
+                    is_ok &= is_saved
+                    msg = fmt2.format(filename) if is_saved else File.message
+
+                lst and lst.append('=' * 20)
+                lst.append(msg)
+
+            lst and Printer.print(lst)
+            sys.exit(ECODE.SUCCESS if is_ok else ECODE.BAD)
         else:
-            user_data = op_txt
+            print(factory.template)
+            sys.exit(ECODE.SUCCESS)
 
-        try:
-            factory = TemplateBuilder(
-                user_data=user_data, author=options.author, email=options.email,
-                company=options.company
-            )
-
-            template_id = options.tmplid.strip()
-            filename = options.filename.strip()
-
-            fmt1 = '+++ Successfully uploaded generated template to "{}" template ID.'
-            fmt2 = '+++ Successfully saved generated template to {}'
-            fmt3 = 'CANT save generated template to existing {} file.  Use replaced flag accordingly.'
-
-            if template_id or filename:
-                is_ok = True
-                lst = []
-                if template_id:
-                    is_uploaded = TemplateStorage.upload(
-                        template_id, factory.template, replaced=options.replaced
-                    )
-                    is_ok &= is_uploaded
-                    msg = fmt1.format(template_id) if is_uploaded else TemplateStorage.message
-                    lst.append(msg)
-                if filename:
-                    filename = File.get_path(filename)
-                    if File.is_exist(filename) and not options.replaced:
-                        msg = fmt3.format(filename)
-                        is_ok &= False
-                    else:
-                        is_saved = File.save(options.filename, factory.template)
-                        is_ok &= is_saved
-                        msg = fmt2.format(filename) if is_saved else File.message
-
-                    lst and lst.append('=' * 20)
-                    lst.append(msg)
-
-                lst and Printer.print(lst)
-                sys.exit(ECODE.SUCCESS if is_ok else ECODE.BAD)
-            else:
-                print(factory.template)
-                sys.exit(ECODE.SUCCESS)
-
-        except Exception as ex:
-            print(Text(ex))
-            sys.exit(ECODE.BAD)
-
-    elif command == 'build' and feature != 'template':
-        if feature == 'script':
-            return
-        else:
-            exit_code = ECODE.SUCCESS if feature == 'usage' else ECODE.BAD
-            show_usage(command, exit_code=exit_code)
+    except Exception as ex:
+        print(Text(ex))
+        sys.exit(ECODE.BAD)
 
 
 def do_search_template(options):
