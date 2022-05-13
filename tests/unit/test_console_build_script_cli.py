c0a35e3649e29d875e061abfc91938ba22ee57e2
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-05-13 02:42:35 -0700

    unittest: add more unit test for test_console_build_script_cli.py

--- tests/unit/test_console_build_script_cli.py
+++ tests/unit/test_console_build_script_cli.py
@@ -2,6 +2,11 @@ import pytest
 
 from dgspoc.utils import File
 from dgspoc.utils import MiscOutput
+from dgspoc.constant import FWTYPE
+
+from os import path
+from tempfile import gettempdir
+from time import time
 
 from . import ReformatOutput
 
@@ -15,6 +20,13 @@ TESTDATA = File.get_result_from_yaml_file(
 
 base_dir = File.get_dir(__file__)
 snippet_filename = File.get_path(base_dir, 'data/snippet_for_script_builder.txt')
+
+
+def get_temp_file(framework):
+    extension = 'robot' if framework == FWTYPE.ROBOTFRAMEWORK else 'py'
+    base_file = 'test_generated_script_%d.%s' % (time(), extension)
+    tmp_file = path.join(gettempdir(), base_file)
+    return tmp_file
 
 
 class TestConsoleBuildScriptCommandLineUsage:
@@ -60,3 +72,32 @@ class TestConsoleBuildTestScript:
         reformat_result = ReformatOutput(result.output, everything=True)
         assert result.is_success
         assert reformat_result.strip() == expected_result
+
+
+class TestConsoleBuildTestScript:
+    @pytest.mark.parametrize(
+        ('cmdline', 'expected_result'),
+        [
+            (TESTDATA.unittest.fmt % snippet_filename, TESTDATA.unittest.expected_result),              # noqa
+            (TESTDATA.pytest.fmt % snippet_filename, TESTDATA.pytest.expected_result),                  # noqa
+            (TESTDATA.robotframework.fmt % snippet_filename, TESTDATA.robotframework.expected_result),  # noqa
+        ]
+    )
+    def test_building_test_script(self, cmdline, expected_result):
+        result = MiscOutput.execute_shell_command(cmdline)
+        reformat_result = ReformatOutput(result.output, everything=True)
+        assert result.is_success
+        assert reformat_result.strip() == expected_result
+
+    @pytest.mark.parametrize(
+        'cmdline',
+        [
+            TESTDATA.fmt_with_save_to % ('unittest', snippet_filename, get_temp_file('unittest')),              # noqa
+            TESTDATA.fmt_with_save_to % ('pytest', snippet_filename, get_temp_file('pytest')),                  # noqa
+            TESTDATA.fmt_with_save_to % ('robotframework', snippet_filename, get_temp_file('robotframework')),  # noqa
+        ]
+    )
+    def test_building_test_script_and_save_to_file(self, cmdline):
+        result = MiscOutput.execute_shell_command(cmdline)
+        assert result.is_success
+        assert 'Successfully saved the generated test script to' in result.output
