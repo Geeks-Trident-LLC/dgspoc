53e70d53245f76013284f7302a11245a4362b73f
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-05-19 14:08:01 -0700

    unittest: add replacing_report_filename_pat for ReformatOutput.process

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -88,6 +88,10 @@ class ReformatOutput:
         replacing_service_stamp_pat = r'(?i)^[a-z]{3} \d{2} \d{4} \d{2}:\d{2}:\d{2}[.]\d{3}'
         replacing_created_date_pat = r'(?i)^(# Created date:) (\d{4}-\d\d-\d\d)( *)$'
 
+        pat = (r'=(?i)(unittest|pytest|robotframework)_(report|output|log)_'
+               r'([0-9]{4}[a-z]{3}[0-9]{2}_[0-9]{6})[.](xml|html)')
+        replacing_report_filename_pat = pat
+
         device_name_pat = r'(?i)(?P<name>\S+) +is +(successfully +)?((dis)?connected)[.]'
         for line in lines:
             if re.match(excluded_txt_pat, line):
@@ -99,6 +103,10 @@ class ReformatOutput:
             elif self.everything and re.match(replacing_created_date_pat, line):
                 changed_txt = re.sub(replacing_created_date_pat,
                                      r'\1 yyyy-mm-dd\3', line)
+                lst.append(changed_txt)
+            elif self.everything and re.match(replacing_report_filename_pat, line):
+                changed_txt = re.sub(replacing_report_filename_pat,
+                                     r'=\1_\2_yyyymmmdd_HHMMSS.\4', line)
                 lst.append(changed_txt)
             else:
                 lst.append(line)
