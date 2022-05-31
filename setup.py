1b4cc98b4e2b66dffb23825eeb1b3447ba155d54
Tuyen Mathew Duong <tuyen@geekstrident.com>
2022-05-31 14:08:29 -0700

    version: update dgspoc to version 0.3.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dgspoc',
-    version='0.3.9',
+    version='0.3.10',
     license='BSD-3-Clause',
     license_files=['LICENSE'],
     description='The proof of concept for Describe-Get-System.',
